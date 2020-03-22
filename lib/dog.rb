46aaf50419b585ec3c67e64574a6d8b24c094ee8
YUKIKO IKEDA <applelele@gmail.com>
2020-03-22 04:11:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each {|each_dog| puts each_dog.name}
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @all = @@all.clear
+  end
+end
