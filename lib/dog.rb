07753cc2b790a2e14c13c9d74bcb9f4077578c2e
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-23 00:06:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+  
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.map do |item|
+      puts item.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+end
+
+# Manotas = Dog.new("Manotas")
+# Lucas = Dog.new("Lucas")
+
